4650f6d1d4d95e8d7b45f6a862dd70b6a7706cc3
Caleb Ness <calebness75@gmail.com>
2019-02-19 16:06:17 -0600

    Made contact field larger

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,13 @@
 var name = "";
-var raise = "I do not raise bottle calves.";
 var message = "";
 function variable1(input) {
   name = "Hello. My name is "+input;
-};
-function variable2(input) {
-  raise = input;
 };
 function variable3(input) {
   message = input;
 };
 
 function mail(){
-  window.open("mailto:calfpacifier@gmail.com?subject=Feedback/Questions&body="+name+" "+message);
+  window.location.href = "mailto:calfpacifier@gmail.com?subject=Feedback/Questions&body="+name+" "+message;
+  alert("Thank You!");
 };
