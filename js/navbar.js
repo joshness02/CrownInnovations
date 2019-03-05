f054e866b1b0ab2f651a43d788091ae22fbfbe58
Joshua <joshness02@icloud.com>
2019-03-05 10:14:52 -0600

    Navbar Edits

---
+++ js/navbar.js
@@ -0,0 +1,21 @@
+(function(){
+    var navbtn = document.getElementById("navbar-dropdown-btn");
+    var navmenu = document.getElementById("navbar-dropdown");
+    navbtn.addEventListener("click", function(event){
+        console.log("Clicked");
+        if(navmenu.classList){
+            navmenu.classList.toggle("active");
+            console.log(navmenu.classList);
+        }else{
+            var classes = navmenu.className.split(" ");
+            var i = classes.indexOf("active");
+            
+            if(i >= 0){
+                classes.splice(i, 1);
+            }else{
+                classes.push("active");
+                navmenu.className = classes.join(" ");
+            }
+        }
+    });
+})();
