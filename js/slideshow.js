a399783a20bb03ace7a8eb7ba0713255fbf34851
Caleb Ness <calebness75@gmail.com>
2019-03-05 12:19:27 -0600

    Added buttons for slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,7 +1,7 @@
 var slideNumber = 1;
 updateSlides(slideNumber);
 function nextSlide(){
-  slideNumber = slideNumber - 1;
+  slideNumber = slideNumber + 1;
   updateSlides(slideNumber);
 };
 function prevSlide(){
@@ -10,14 +10,14 @@ function prevSlide(){
 };
 function updateSlides(n){
   var slides = document.getElementById('slide');
-  if(slideNumber<1){
+  if(n<1){
     slideNumber = slides.length;
   }
-  if(slideNumber>slides.length){
+  if(n>slides.length){
     slideNumber = 1;
   }
   for(var i = 0; i < slides.length; i++){
     slides[i].style.display = "none";
   }
-  slides[n].style.display = inline;
+  slides[slideNumber-1].style.display = block;
 };
