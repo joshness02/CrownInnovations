2cb5577d66be9529fc5b312cd0417784a08fa628
Joshua <joshness02@icloud.com>
2019-04-01 15:29:33 -0500

    Fixed Blog

--- php/contact.php
+++ php/contact.php
@@ -3,18 +3,17 @@
     $email = $_POST['email'];
     $message = $_POST['message'];
     $from = 'From: Calf Pacifier Customer';
-    $to = 'joshness02@icloud.com';
+    $to = 'calfpacifier@gmail.com';
     $subject = 'Customer Message';
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
     if($_POST['submit']){
-        /*
-        if(){
+        if(echo mail($to, $subject, $body, $from)){
             echo '<p>Your Message Has Been Sent</p>';
         }else{
             echo '<p>We are very sorry, but we encountered a problem.</p>';
-        }*/
-        echo mail($to, $subject, $body, $from);
+        }
+        
     }
 ?>
