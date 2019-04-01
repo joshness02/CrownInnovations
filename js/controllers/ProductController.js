657838490c514e826f875220be92a624f32f825e
Joshua <joshness02@icloud.com>
2019-04-01 16:30:29 -0500

    Fixed image paths to work with github pages

--- js/controllers/ProductController.js
+++ js/controllers/ProductController.js
@@ -1,7 +1,7 @@
 var products = [
     {
         name: "Stationary Calf Pacifier for Wooden Fences",
-        img: "../../imgs/StationaryFencePicture.png",
+        img: "./../../../imgs/StationaryFencePicture.png",
         description: "Great for longer periods of time when you may not be with you calf",
         about: "The Stationary Calf Pacifier can hang on most wooden and round metal fences (For other options, check out our Stationary Calf Pacifier Wrap Around). It is very durable, so it can be left with the calves when you are away.",
         price: 20,
@@ -10,7 +10,7 @@ var products = [
     },
     {
         name: "Stationary Calf Pacifier Wrap Around",
-        img: "../../imgs/StationaryWrapAroundPicture.png",
+        img: "./../../../imgs/StationaryWrapAroundPicture.png",
         description: "Can be easily secured anywhere you need it",
         about: "The Stationary Calf Pacifier Wrap Around is designed with adaption in mind, so you can secure it anywhere you need it. Whether you have wire fencing, cattle panels, or wooden fences, the Stationary Calf Pacifier Wrap Around will attach securely, no problem.",
         price: 20,
@@ -19,7 +19,7 @@ var products = [
     },
     {
         name: "Mobile Calf Pacifier",
-        img: "../../imgs/MobilePicture.png",
+        img: "./../../../imgs/MobilePicture.png",
         description: "Great for one-on-one time with your bottle calves",
         about: "The Mobile Calf Pacifier is great for when you are spending time with your calf. Whether you are trying to keep him occupied while you feed other calves, or just having fun with him, the Mobile Calf Pacifier is here to help. If the Pacifier is dropped on the ground, it will automatically right itself using our Patented Pop-Up design, keeping the Pacifier safe and clean.",
         price: 15,
