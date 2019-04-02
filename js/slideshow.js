146cffd745cf724c29e3da5c2947ef2ed0650fce
Caleb Ness <calebness75@gmail.com>
2019-04-02 10:42:43 -0500

    Fixed slideshow

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,28 +1,29 @@
-var slideNumber = 1;
-var time;
+var slideNumber = 0;
+
+function updateSlides(n) {
+    var slides = document.getElementsByClassName('slide');
+    if (n < 1) {
+        slideNumber = slides.length;
+    }
+    if (n > slides.length) {
+        slideNumber = 1;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[slideNumber - 1].style.opacity = "0%";
+    }
+    slides[slideNumber - 1].style.display = "block";
+    slides[slideNumber - 1].style.opacity = "100%";
+};
+
 updateSlides(slideNumber);
-function nextSlide(){
-  slideNumber = slideNumber + 1;
-  updateSlides(slideNumber);
-  clearTimeout(time);
+
+function nextSlide() {
+    slideNumber = slideNumber + 1;
+    updateSlides(slideNumber);
 };
-function prevSlide(){
-  slideNumber = slideNumber - 1;
-  updateSlides(slideNumber);
-  clearTimeout(time);
-};
-function updateSlides(n){
-  var slides = document.getElementsByClassName('slide');
-  if(n<1){
-    slideNumber = slides.length;
-  }
-  if(n>slides.length){
-    slideNumber = 1;
-  }
-  for(var i = 0; i < slides.length; i++){
-    slides[i].style.display = "none";
-    slides[slideNumber-1].style.opacity = "0%";
-  }
-  slides[slideNumber-1].style.display = "block";
-  slides[slideNumber-1].style.opacity = "100%";
+
+function prevSlide() {
+    slideNumber = slideNumber - 1;
+    updateSlides(slideNumber);
 };
